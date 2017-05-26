acceb7dceb4401b0598f6899e2293ee3d68dd56e
semistone222 <semistone222@gmail.com>
2017-05-26 10:34:47 +0900

    매월 상환액 계산

--- service/estimate.js
+++ service/estimate.js
@@ -50,10 +50,14 @@ class Estimate {
     getEstimateByEstimateId(estimate) {
         return new Promise((resolve, reject) => {
             pool.getConnection().then((conn) => {
-                const sql = 'SELECT * FROM estimate, agent WHERE estimate.agent_id = agent.agent_id AND estimate.estimate_id = ?';
+                const sql = 'SELECT * FROM estimate, agent, request WHERE estimate.agent_id = agent.agent_id AND estimate.request_id = request.request_id AND estimate.estimate_id = ?';
                 conn.query(sql, [estimate.estimateId]).then(results => {
                     pool.releaseConnection(conn);
-                    resolve(results);
+                    this.getRepaymentPerMonth(results[0]).then(ret => {
+                        console.log(ret);
+                        results[0]["repayment_amount_per_month"] = ret;
+                        resolve(results);
+                    });
                 });
             }).catch((err) => {
                 reject(err);
@@ -116,6 +120,25 @@ class Estimate {
                     reject(err);
                 });
             });
+        });
+    }
+
+    /**
+     * 원리금 균등 상환
+     * 매월 상환액 계산하기
+     * A = 대출원금
+     * b = 대출이자율, 즉 연이자율/12
+     * n = 상환기간, 즉 실제상환개월수
+     * @param estimate
+     * @returns {Promise}
+     */
+    getRepaymentPerMonth(estimate) {
+        return new Promise((resolve, reject) => {
+            const A = parseInt(estimate.loan_amount);
+            const b = (parseFloat(estimate.interest_rate) / 100) / 12;
+            const n = parseInt(estimate.loan_period) * 12;
+            const ret = A * b * Math.pow(1 + b, n) / (Math.pow(1 + b, n) - 1);
+            resolve(ret);
         });
     }
 }
