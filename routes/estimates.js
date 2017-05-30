a2bec9d32132a6da5446ff568f86287b08296fb2
semistone222 <semistone222@gmail.com>
2017-05-30 09:09:52 +0900

    유효성 검증 및 응답 메시지 수정 (윤준석 파트만) (#22)

--- routes/estimates.js
+++ routes/estimates.js
@@ -5,15 +5,15 @@ const Request = require('../model/request');
 const estimateService = require('../service/estimate');
 
 router.route('/estimates')
-    .get(getEstimatesByRequestId)
-    .post(writeEstimate);
+    .get(getEstimatesByRequestId);
+// .post(writeEstimate);
 
 router.route('/estimates/calculate')
     .get(getEstimatesCountAndAvgInterest);
 
 router.route('/estimates/:estimateId')
-    .get(getEstimateByEstimateId)
-    .put(editEstimate);
+    .get(getEstimateByEstimateId);
+// .put(editEstimate);
 
 /**
  * 요청서 상세 화면에서
@@ -24,12 +24,17 @@ router.route('/estimates/:estimateId')
  */
 function getEstimatesCountAndAvgInterest(req, res, next) {
     const requestId = parseInt(req.query.requestId);
+    if (typeof requestId != 'number' || isNaN(requestId)) {
+        res.send({msg: 'wrong parameters'});
+        return;
+    }
+
     const request = new Request(requestId);
 
     estimateService.getEstimatesCountAndAvgInterest(request).then(results => {
         res.send({msg: 'success', data: results});
     }).catch(error => {
-        res.send({msg: 'failed'});
+        res.send({msg: error});
     });
 }
 
@@ -42,12 +47,17 @@ function getEstimatesCountAndAvgInterest(req, res, next) {
  */
 function getEstimatesByRequestId(req, res, next) {
     const requestId = parseInt(req.query.requestId);
+    if (typeof requestId != 'number' || isNaN(requestId)) {
+        res.send({msg: 'wrong parameters'});
+        return;
+    }
+
     const request = new Request(requestId);
 
     estimateService.getEstimatesByRequestId(request).then(results => {
         res.send({msg: 'success', data: results});
     }).catch(error => {
-        res.send({msg: 'failed'});
+        res.send({msg: error});
     });
 }
 
@@ -61,69 +71,76 @@ function getEstimatesByRequestId(req, res, next) {
  */
 function getEstimateByEstimateId(req, res, next) {
     const estimateId = parseInt(req.params.estimateId);
+    if (typeof estimateId != 'number' || isNaN(estimateId)) {
+        res.send({msg: 'wrong parameters'});
+        return;
+    }
+
+    console.log(estimateId);
     const estimate = new Estimate(estimateId);
 
     estimateService.getEstimateByEstimateId(estimate).then(results => {
         res.send({msg: 'success', data: results});
     }).catch(error => {
-        res.send({msg: 'failed'});
+        res.send({msg: error});
     });
 }
 
-function writeEstimate(req, res, next) {
-    const body = req.body;
-    const estimate = new Estimate(
-        null,
-        body.requestId,
-        body.agentId,
-        null,
-        body.itemBank,
-        body.itemName,
-        body.interestRate,
-        body.interestRateType,
-        body.repaymentType,
-        body.overdueInterestRate1,
-        body.overdueInterestRate2,
-        body.overdueInterestRate3,
-        body.overdueTime1,
-        body.overdueTime2,
-        body.overdueTime3,
-        body.earlyRepaymentFee
-    );
+/*
+ function writeEstimate(req, res, next) {
+ const body = req.body;
+ const estimate = new Estimate(
+ null,
+ body.requestId,
+ body.agentId,
+ null,
+ body.itemBank,
+ body.itemName,
+ body.interestRate,
+ body.interestRateType,
+ body.repaymentType,
+ body.overdueInterestRate1,
+ body.overdueInterestRate2,
+ body.overdueInterestRate3,
+ body.overdueTime1,
+ body.overdueTime2,
+ body.overdueTime3,
+ body.earlyRepaymentFee
+ );
 
-    estimateService.writeEstimate(estimate).then(results => {
-        res.send({msg: 'success', status: results});
-    }).catch(error => {
-        res.send({msg: 'failed'});
-    });
-}
+ estimateService.writeEstimate(estimate).then(results => {
+ res.send({msg: 'success', status: results});
+ }).catch(error => {
+ res.send({msg: 'failed'});
+ });
+ }
 
-function editEstimate(req, res, next) {
-    const body = req.body;
-    const estimate = new Estimate(
-        req.params.estimateId,
-        body.requestId,
-        body.agentId,
-        null,
-        body.itemBank,
-        body.itemName,
-        body.interestRate,
-        body.interestRateType,
-        body.repaymentType,
-        body.overdueInterestRate1,
-        body.overdueInterestRate2,
-        body.overdueInterestRate3,
-        body.overdueTime1,
-        body.overdueTime2,
-        body.overdueTime3,
-        body.earlyRepaymentFee
-    );
+ function editEstimate(req, res, next) {
+ const body = req.body;
+ const estimate = new Estimate(
+ req.params.estimateId,
+ body.requestId,
+ body.agentId,
+ null,
+ body.itemBank,
+ body.itemName,
+ body.interestRate,
+ body.interestRateType,
+ body.repaymentType,
+ body.overdueInterestRate1,
+ body.overdueInterestRate2,
+ body.overdueInterestRate3,
+ body.overdueTime1,
+ body.overdueTime2,
+ body.overdueTime3,
+ body.earlyRepaymentFee
+ );
 
-    estimateService.editEstimate(estimate).then(results => {
-        res.send({msg: 'success', status: results});
-    }).catch(error => {
-        res.send({msg: 'failed'});
-    });
-}
+ estimateService.editEstimate(estimate).then(results => {
+ res.send({msg: 'success', status: results});
+ }).catch(error => {
+ res.send({msg: 'failed'});
+ });
+ } */
 
 module.exports = router;
