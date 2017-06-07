d246649b54e8419d430d274e1a1e488e2f3d64ce
Len <len@Lenui-MacBook-Pro.local>
2017-06-07 17:25:49 +0900

    #요청서에 대한 견적서 추가 기능 완성, 견적서 알림 기능 추가

---
+++ routes/consultant/request.js
@@ -0,0 +1,132 @@
+/**
+ * Created by len on 2017. 6. 6..
+ */
+const express = require('express');
+const router = express.Router();
+const requestService = require('../../service/request');
+const Estimate = require('../../model/esimate');
+const fcm = require('../../config/fcm');
+const schedule = require('node-schedule');
+
+
+
+
+// router.route('/requests/calculate')
+//     .get(getCalculatedRequestCountByStatus);
+
+
+router.route('/requests/agent/:agentId')
+    .get(getRequests)
+    .post(setRequest);
+
+router.route('/requests/:requestId/:agentId')
+    .get(getRequestByRequestId);
+
+
+// async function getCalculatedRequestCountByStatus(req, res, next) {
+//
+//     const agentId = req.body.agentId;
+//
+//     try {
+//         const results =  await requestService.getRequestConsultantRequestByStatus(agentId);
+//             res.send({msg: 'success', data: results});
+//         }catch (err){
+//             res.send({err : err})
+//     }
+// }
+
+async function setRequest(req, res, next) {
+
+    const agentId = req.params.agentId;
+    const body = req.body;
+
+    const estimate = new Estimate(
+        null,
+        body.requestId,
+        agentId,
+        null,
+        body.itemBank,
+        body.itemName,
+        body.interestRate,
+        body.interestRateType,
+        body.repaymentType,
+        body.overdueInterestRate01,
+        body.overdueInterestRate02,
+        body.overdueInterestRate03,
+        body.overdueTime01,
+        body.overdueTime02,
+        body.overdueTime03,
+        body.earlyRepaymentFee
+    );
+
+    try {
+        const addEstimate = await requestService.addEstimateIntoRequest(estimate);
+        const countRequest = await requestService.getRequestConsultantByRequestID(body.requestId, agentId);
+        const customer = await requestService.getCustomerIdAndToken(body.requestId);
+
+        if(countRequest[0].estimate_count === 5){
+
+            if(customer[0].fcm_token.length === 0){
+                res.status(400).send({err : 'There is no Any fcm_token For Push'});
+                return;
+            }
+
+            await fcm.sendNotification(customer[0].fcm_token, '견적서 알림', '현재 견적서가 5개 입니다.');
+
+            console.log(customer[0].customer_id + "에게" + "토큰 번호 :" + customer[0].fcm_token + "으로 5개 알림 전송 완료");
+
+            res.send({ msg : 'success', data: addEstimate});
+
+        }else if(countRequest[0].estimate_count === 10){
+
+            if(customer[0].fcm_token.length === 0){
+                res.status(400).send({err : 'no fcm_token'});
+                return;
+            }
+
+            await fcm.sendNotification(customer[0].fcm_token, '견적서 알림', '현재 견적서가 10개로 마감되었습니다.');
+            console.log(customer[0].customer_id + "에게" + "토큰 번호 :"+ customer[0].fcm_token + "으로 10개 알림 전송 완료");
+
+            requestService.finishRequest(body.requestId, "견적마감");
+            console.log(customer[0].customer_id + "에 대한 " +'견적마감');
+
+            const scheduled = schedule.scheduledJobs;
+            if (scheduled[body.requestId.toString()] !== null)
+                scheduled[body.requestId.toString()].cancel();
+            res.send({ msg : 'success', data: addEstimate});
+        } else {
+        res.send({ msg : 'success', data: addEstimate});
+        }
+
+    } catch (err){
+        res.send({err:err})
+    }
+}
+
+async function getRequests(req, res, next) {
+
+    const agentId = req.params.agentId;
+
+    try {
+        const results = await requestService.getRequestConsultantRequestByStatus(agentId);
+
+        res.send({ msg : 'success', data: results});
+    }catch (err){
+        res.send({err:err})
+    }
+}
+
+async function getRequestByRequestId(req, res, next) {
+
+    const requestId = req.params.requestId;
+    const agentId = req.params.agentId;
+    //TODO 클라이언트단에서 만약 갯수가 2개면 안보인다고 말하기.
+    try {
+        const results = await requestService.getRequestConsultantByRequestID(requestId, agentId);
+        res.send({msg: 'success', data : results})
+    }catch (err){
+        res.send({err: err})
+    }
+}
+
+module.exports = router;
--- service/request.js
+++ service/request.js
@@ -184,6 +184,161 @@ class Request {
             });
         });
     }
+
+    ///////////////////////대출모집용///////////////////////////
+
+
+    // SELECT req.loan_type, req.end_time,
+    // req.region_1, req.region_2, req.region_3,
+    // req.apt_name,apt_size_supply, req.apt_size_exclusive,
+    // req.loan_amount,req.job_type,req.overdue_record, if (isnull(li.request_id), 0, 1)
+    // FROM request AS req, like_request AS li, estimate AS es
+    // WHERE es.request_id = req.selected_estimate_id AND req.request_id = li.request_id;
+    //
+    // SELECT count(es.request_id)FROM request AS req, like_request AS li, estimate AS es
+    // WHERE es.request_id = req.selected_estimate_id AND req.request_id = li.request_id;
+
+    /**
+     *
+     * @returns {Promise}
+     * @param agentId
+     */
+    getRequestConsultantRequestByStatus(agentId){
+        return new Promise((resolve, reject) => {
+            pool.getConnection().then((conn) => {
+
+                const sql = 'select '+
+                'count(estimate.estimate_id) as count,' +
+                'if((select' +
+                    ' like_request.register_time' +
+                ' from like_request ' +
+                'where 1=1 ' +
+                'and like_request.agent_id = ? ' +
+                'and like_request.request_id = request.request_id), 1, 0) as favorite, ' +
+                'request.*, ' +
+                'estimate.* ' +
+                'from request, estimate ' +
+                'where 1=1 ' +
+                'and request.request_id = estimate.request_id ' +
+                'group by request.request_id ';
+
+                conn.query(sql, [agentId]).then((results) => {
+                    pool.releaseConnection(conn);
+
+                    if(results.length === 0){
+                        reject("no data");
+                        return;
+                    }
+                    resolve(results);
+                });
+            }).catch((err) => {
+                reject(err);
+            })
+        })
+    }
+
+    addEstimateIntoRequest(estimate){
+        return new Promise((resolve, reject) => {
+            pool.getConnection().then((conn) => {
+
+                const sql= "INSERT INTO estimate (request_id, agent_id, item_bank," +
+                    "item_name, interest_rate, interest_rate_type, repayment_type, " +
+                    "overdue_interest_rate_1, overdue_inertest_rate_2, overdue_inertest_rate_3," +
+                    "overdue_time_1,overdue_time_2,overdue_time_3,early_repayment_fee) " +
+                    "VALUES ( ?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+                conn.query(sql, [estimate.requestId, estimate.agentId, estimate.itemBank,
+                    estimate.itemName, estimate.interestRate, estimate.interestRateType,
+                    estimate.repaymentType, estimate.overdueInterestRate01, estimate.overdueInterestRate02,
+                    estimate.overdueInterestRate03, estimate.overdueTime01, estimate.overdueTime02, estimate.overdueTime03, estimate.earlyRepaymentFee]).then((results) => {
+                    pool.releaseConnection(conn);
+
+                    if(results.length === 0){
+                        reject({msg: "No date"});
+                        return;
+                    }
+                    console.log( estimate.agentId +"가 " + estimate.requestId +"에 대한 견적서 작성 완료");
+                    resolve(results);
+                })
+            });
+        }).catch((err) => {
+            reject(err);
+        })
+    }
+
+    /**
+     * 한명의 상담가는 단 하나의 견적서만을 작성할 수 있다.
+     * @param requestId
+     * @param agentId
+     * @returns {Promise}
+     */
+    getRequestConsultantByRequestID(requestId, agentId ){
+        return new Promise((resolve, reject) => {
+            pool.getConnection().then((conn) => {
+
+                const sql =  'select ' +
+                'count(estimate.estimate_id) as estimate_count,' +
+                    '(select count(ag.company_name)' +
+                'from agent ag, estimate es ' +
+                'where 1=1 ' +
+                'and ag.company_name = (select a.company_name from agent a where a.agent_id = ?) ' +
+                'and ag.agent_id = es.agent_id ' +
+                'and es.request_id = request.request_id group by ag.company_name ' +
+                ') as bank_count, ' +
+                'if((select ' +
+                    'like_request.register_time ' +
+                'from like_request ' +
+                'where 1=1 ' +
+                'and like_request.agent_id = ? ' +
+                'and like_request.request_id = request.request_id), 1, 0 ' +
+                ') as favorite, ' +
+                'request.*, ' +
+                'estimate.* ' +
+                'from request, estimate ' +
+                'where 1=1 ' +
+                'and request.request_id = estimate.request_id ' +
+                'and request.request_id = ? ' +
+                'group by estimate.request_id;';
+
+                conn.query(sql, [agentId, agentId,requestId]).then((results) => {
+                    pool.releaseConnection(conn);
+
+                    if(results.length === 0){
+                        reject("no data");
+                        return;
+                    }
+
+                    resolve(results);
+                });
+            }).catch((err) => {
+                reject(err);
+            })
+        })
+    }
+
+    getCustomerIdAndToken(requestId){
+        return new Promise((resolve, reject) => {
+            pool.getConnection().then((conn) => {
+
+                const sql = 'SELECT cu.customer_id, cu.fcm_token ' +
+                'FROM customer AS cu, request AS req ' +
+                'WHERE cu.customer_id = req.customer_id ' +
+                'AND req.request_id = ?';
+
+                conn.query(sql, [requestId]).then((results) => {
+                    pool.releaseConnection(conn);
+                    resolve(results)
+                })
+            }).catch((err) => {
+
+                reject({err:err});
+
+                }
+            )
+        })
+    }
+
 }
+
 
 module.exports = new Request();
