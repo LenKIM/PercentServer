8c4add34a04e3a17e9d5f1cbcc0477c9d7b7817c
semistone222 <semistone222@gmail.com>
2017-06-15 16:20:26 +0900

    ITEM API 일부 수정

--- routes/consultant/items.js
+++ routes/consultant/items.js
@@ -1,43 +1,42 @@
 const express = require('express');
 const router = express.Router();
 const Agent = require('../../model/agent');
-const Pager = require('../../model/pager');
 const Item = require('../../model/item');
 const itemService = require('../../service/item');
 
-router.route('/:agentId/items')
-    .get(showItemList)
-    .put(addItem);
+router.route('/items')
+    .get(getItemsByAgentId)
+    .post(addItem);
 
 router.route('/items/:itemId')
     .get(showDetailItem)
     .put(editItem)
     .delete(deleteItem);
+
 /**
  * 대출 상품 리스트 목록 조회
  */
-function showItemList(req, res, next) {
+async function getItemsByAgentId(req, res, next) {
     const page = parseInt(req.query.page) || 1;
     const count = parseInt(req.query.count) || 30;
-    const agentId = req.params.agentId;
-    const pager = new Pager(page, count);
-    const agent = new Agent(agentId);
+    const agentId = req.query.agentId;
 
-    itemService.getItems(agent, pager).then(results => {
-        res.send({msg:'SUCCESS', paging : results.paging, data: results.data });
-    }).catch(err => {
-        next(err)
-    });
-
+    try {
+        const results = await itemService.getItems(agentId, page, count);
+        res.send({msg: 'SUCCESS', paging : results.paging, data: results.data });
+    } catch (error) {
+        next(error);
+    }
 }
 
 /**
  * 대출 모집인이 대출 상품 등록 할때 사용하는 API
  */
-function addItem(req, res, next) {
-
+async function addItem(req, res, next) {
     const body = req.body;
-    const agentId = new Agent(req.params.agentId);
+    const agent = new Agent(
+        body.agentId
+    );
     const item = new Item(
         null,
         body.itemBank,
@@ -55,34 +54,35 @@ function addItem(req, res, next) {
         body.earlyRepaymentFee
     );
 
-    itemService.addItem(item, agentId).then(results => {
-        res.send({msg: 'SUCCESS', status: results})
-    }).catch(err => {
-        next(err);
-    });
+    try {
+        const results = await itemService.addItem(item, agent);
+        res.send({msg: 'SUCCESS'});
+    } catch (error) {
+        next(error);
+    }
 }
 
 /**
  * 특정 상품 정보 조회
  */
-function showDetailItem(req, res, next) {
-
+async function showDetailItem(req, res, next) {
     const itemId = req.params.itemId;
 
-    itemService.getItem(itemId).then(results => {
+    try {
+        const results = await itemService.getItem(itemId);
         res.send({msg: 'SUCCESS', data: results});
-    }).catch(err => {
-        next(err);
-    });
+    } catch (error) {
+        next(error);
+    }
 }
 
 /**
  * 특정 상품 정보 수정
  */
-function editItem(req, res, next) {
+async function editItem(req, res, next) {
     const body = req.body;
     const item = new Item(
-        body.itemId,
+        req.params.itemId,
         body.itemBank,
         body.itemName,
         body.minInterestrate,
@@ -98,25 +98,26 @@ function editItem(req, res, next) {
         body.earlyRepaymentFee
     );
 
-    itemService.updateItem(item).then(results => {
-        res.send({msg: 'SUCCESS', status: results});
-    }).catch(err => {
-        next(err)
-    });
+    try {
+        const results = await itemService.updateItem(item);
+        res.send({msg: 'SUCCESS'});
+    } catch (error) {
+        next(error);
+    }
 }
 
 /**
  * 특정 상품 정보 삭제
  */
-function deleteItem(req, res, next) {
-
+async function deleteItem(req, res, next) {
     const itemId = req.params.itemId;
 
-    itemService.deleteitem(itemId).then(results => {
-        res.send({msg: 'SUCCESS', status: results});
-    }).catch(err => {
-        next(err)
-    });
+    try {
+        const results = await itemService.deleteItem(itemId);
+        res.send({msg: 'SUCCESS'});
+    } catch (error) {
+        next(error);
+    }
 }
 
 module.exports = router;
--- service/item.js
+++ service/item.js
@@ -1,16 +1,15 @@
 const pool = require('../config/mysql');
 
 class Item {
-    constructor() {
-    }
-
     getItem(itemId) {
         return new Promise((resolve, reject) => {
             pool.getConnection().then((conn) => {
-                const sql = 'SELECT * FROM item AS it, agent AS ag WHERE it.agent_id = ag.agent_id AND it.item_id =?';
+                const sql = 'SELECT * FROM item AS it, agent AS ag WHERE it.agent_id = ag.agent_id AND it.item_id = ?';
                 conn.query(sql, [itemId]).then(results => {
                     pool.releaseConnection(conn);
-                    resolve('QUERY_ERR');
+                    resolve(results[0]);
+                }).catch((err) => {
+                    reject('QUERY_ERR');
                 });
             }).catch((err) => {
                 reject('CONNECTION_ERR');
@@ -18,28 +17,23 @@ class Item {
         });
     }
 
-    getItems(agent, pager) {
+    getItems(agentId, page, count) {
         return new Promise((resolve, reject) => {
             pool.getConnection().then((conn) => {
-
                 const countSql = 'SELECT count(*) as count FROM item, agent WHERE item.agent_id = agent.agent_id AND agent.agent_id = ?';
-                conn.query(countSql, [agent.agentId]).then(results => {
-
+                conn.query(countSql, [agentId]).then(results => {
                     const totalCount = parseInt(results[0].count);
-                    const maxPage = Math.floor(totalCount / pager.count);
-                    const offset = pager.count * (pager.page - 1);
-
+                    const maxPage = Math.floor(totalCount / count);
+                    const offset = count * (page - 1);
                     const sql = 'SELECT * FROM item, agent WHERE item.agent_id = agent.agent_id AND agent.agent_id = ? LIMIT ? OFFSET ?';
-                    conn.query(sql, [agent.agentId, pager.count, offset]).then(results => {
+                    conn.query(sql, [agentId, count, offset]).then(results => {
                         pool.releaseConnection(conn);
-
                         let paging = {
                             total: totalCount,
                             maxPage: maxPage,
-                            page: pager.page,
-                            count: pager.count
+                            page: page,
+                            count: count
                         };
-
                         resolve({
                             paging: paging,
                             data: results
@@ -47,10 +41,12 @@ class Item {
                     }).catch(err => {
                         reject('QUERY_ERR')
                     });
+                }).catch(err => {
+                    reject('QUERY_ERR')
                 });
             }).catch(err => {
                 reject('CONNECTION_ERR')
-            })
+            });
         });
     }
 
@@ -64,59 +60,68 @@ class Item {
         return new Promise((resolve, reject) => {
             pool.getConnection().then(conn => {
                 pool.releaseConnection(conn);
-
-                const sql = 'INSERT INTO item (agent_id, item_bank, item_name, ' +
-                    'min_interest_rate, max_interest_rate, interest_rate_type, ' +
-                    'repayment_type, overdue_interest_rate_1, overdue_interest_rate_2, ' +
-                    'overdue_interest_rate_3, overdue_time_1, overdue_time_2, ' +
-                    'overdue_time_3, early_repayment_fee) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
-
-                conn.query(sql, [agent.agentId, item.itemBank, item.itemName,
-                    item.minInterestrate, item.maxInterestrate, item.interestRateType,
-                    item.repaymentType, item.overdueInterestRate01, item.overdueInterestRate02,
-                    item.overdueInterestRate03, item.overdueTime01, item.overdueTime02, item.overdueTime03,
-                    item.earlyRepaymentFee]).then(results => {
+                const sql = 'INSERT INTO item (agent_id, item_bank, item_name, min_interest_rate, max_interest_rate, interest_rate_type, repayment_type, overdue_interest_rate_1, overdue_interest_rate_2, overdue_interest_rate_3, overdue_time_1, overdue_time_2, overdue_time_3, early_repayment_fee) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
+                conn.query(sql, [
+                    agent.agentId,
+                    item.itemBank,
+                    item.itemName,
+                    item.minInterestrate,
+                    item.maxInterestrate,
+                    item.interestRateType,
+                    item.repaymentType,
+                    item.overdueInterestRate01,
+                    item.overdueInterestRate02,
+                    item.overdueInterestRate03,
+                    item.overdueTime01,
+                    item.overdueTime02,
+                    item.overdueTime03,
+                    item.earlyRepaymentFee
+                ]).then(results => {
                     resolve(results);
                 }).catch(err => {
                     reject('QUERY_ERR');
                 });
             }).catch(err => {
-                reject('CONNECTION_ERR')
+                reject('CONNECTION_ERR');
             });
         });
     }
 
-    updateItem(item, agent) {
+    updateItem(item) {
         return new Promise((resolve, reject) => {
             pool.getConnection().then(conn => {
                 pool.releaseConnection(conn);
-
-                const sql = 'UPDATE item SET item_bank = ?, item_name = ?, ' +
-                    'min_interest_rate = ?, max_interest_rate = ?, interest_rate_type = ?, ' +
-                    'repayment_type = ?, overdue_interest_rate_1 = ?, overdue_interest_rate_2 = ?, ' +
-                    'overdue_interest_rate_3 = ?, overdue_time_1 = ?, overdue_time_2 = ?, ' +
-                    'overdue_time_3 = ?, early_repayment_fee = ? WHERE item_id = ?';
-
-                conn.query(sql, [item.itemBank, item.itemName,
-                    item.minInterestrate, item.maxInterestrate, item.interestRateType,
-                    item.repaymentType, item.overdueInterestRate01, item.overdueInterestRate02,
-                    item.overdueInterestRate03, item.overdueTime01, item.overdueTime02, item.overdueTime03,
-                    item.earlyRepaymentFee, item.itemId]).then(results => {
-
+                const sql = 'UPDATE item SET item_bank = ?, item_name = ?, min_interest_rate = ?, max_interest_rate = ?, interest_rate_type = ?, repayment_type = ?, overdue_interest_rate_1 = ?, overdue_interest_rate_2 = ?, overdue_interest_rate_3 = ?, overdue_time_1 = ?, overdue_time_2 = ?, overdue_time_3 = ?, early_repayment_fee = ? WHERE item_id = ?';
+                conn.query(sql, [
+                    item.itemBank,
+                    item.itemName,
+                    item.minInterestrate,
+                    item.maxInterestrate,
+                    item.interestRateType,
+                    item.repaymentType,
+                    item.overdueInterestRate01,
+                    item.overdueInterestRate02,
+                    item.overdueInterestRate03,
+                    item.overdueTime01,
+                    item.overdueTime02,
+                    item.overdueTime03,
+                    item.earlyRepaymentFee,
+                    item.itemId
+                ]).then(results => {
                     resolve(results);
                 }).catch(err => {
-                    reject('QUERY_ERR')
+                    reject('QUERY_ERR');
                 });
             }).catch(err => {
-                reject('CONNECTION_ERR')
+                reject('CONNECTION_ERR');
             });
         });
     }
 
-    deleteitem(itemId) {
+    deleteItem(itemId) {
         return new Promise((resolve, reject) => {
             pool.getConnection().then(conn => {
-                const sql = 'DELETE FROM item WHERE item_id =?';
+                const sql = 'DELETE FROM item WHERE item_id = ?';
                 conn.query(sql, [itemId]).then(results => {
                     pool.releaseConnection(conn);
                     resolve(results);
@@ -124,7 +129,7 @@ class Item {
                     reject('QUERY_ERR');
                 });
             }).catch(err => {
-                reject('CONNECTION_ERR')
+                reject('CONNECTION_ERR');
             });
         });
     }
