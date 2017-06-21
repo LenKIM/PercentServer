bc57099f3ce732045d924b63f6517adcf82bfe3d
Len <joenggyu0@gmail.com>
2017-06-21 10:45:59 +0900

    # MySQL Config 설정 변경
    MySQL Config 설정 변경

--- config/mysql.js
+++ config/mysql.js
@@ -5,10 +5,10 @@
 var mysql =  require('promise-mysql');
 
 var dbConfig = {
-    host : 'hellomoney2.cj0dugpahfcd.ap-northeast-2.rds.amazonaws.com',
-    user : 'root',
-    password : 'hello1234',
-    port : '3306',
+    host : '',
+    user : '',
+    password : '',
+    port : '',
     database : 'hellomoney'
 };
 
